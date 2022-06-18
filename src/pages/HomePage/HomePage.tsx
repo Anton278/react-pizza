05f0bd9ee84ba723e160bf56569da0d7b2ba1388
anton278 <nakonechnyi.an@gmail.com>
2022-06-18 15:40:29 +0300

    Deleted default sorts when rendering a component; Corrected layout.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,17 +2,10 @@ import "./HomePage.css";
 import Header from "../../components/Header";
 import Nav from "../../components/Nav";
 import PizzaBlock from "../../components/PizzaBlock";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { IPizza } from "../../assets/types";
-import {
-    SORT_BY_POPULARITY,
-    SET_ACTIVE_SORT,
-    SET_ACTIVE_CATEGORY,
-} from "../../actions";
 
 const HomePage = () => {
-    const dispatch = useDispatch();
     const pizza: Array<IPizza> = useSelector(
         (state: any) => state.pizzaReducer
     );
@@ -23,12 +16,6 @@ const HomePage = () => {
         (state: any) => state.appReducer.activeCategory
     );
 
-    useEffect(() => {
-        dispatch({ type: SORT_BY_POPULARITY });
-        dispatch({ type: SET_ACTIVE_SORT, payload: "популярности" });
-        dispatch({ type: SET_ACTIVE_CATEGORY, payload: "Все" });
-    }, []);
-
     return (
         <div className="wrapp">
             {!isLoading && (
@@ -36,8 +23,7 @@ const HomePage = () => {
                     <Header showBasket />
                     <Nav />
                     <main>
-                        <div className="main__title">Все пиццы</div>
-                        <div className="main__pizza-wrapp">
+                        <div className="pizza-wrapp">
                             {pizza
                                 .filter((pizza: IPizza) =>
                                     pizza.category.some(
